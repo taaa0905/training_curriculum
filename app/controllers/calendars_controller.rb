b2df3dd7848ed6f0a57cd3648edd70f17b6ba1c1
taaa0905 <takashinakano0905@gmail.com>
2022-01-13 16:50:02 +0900

    Issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -35,14 +35,20 @@ class CalendarsController < ApplicationController
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
 
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
       wday_num = Date.today.wday + x# wdayメソッドを用いて取得した数値
       if wday_num >= 7#「wday_numが7以上の場合」という条件式
         wday_num = wday_num -7
       end
 
       days = { month: (@todays_date + x).month, date: (@todays_date + x).day, plans: today_plans, wday: wdays[wday_num]}#wdaysから値を取り出す記述
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
       @week_days.push(days)
     end
 
